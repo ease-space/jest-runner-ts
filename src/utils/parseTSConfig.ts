f16c65502f96974bf608d8cad5cce4457b606479
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-02 17:43:06 +0300

    save

--- src/utils/parseTSConfig.ts
+++ src/utils/parseTSConfig.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import ts from 'typescript';
 
 const parseTSConfig = (
-  rootDir: string,
+  rootDir: string = process.cwd(),
   tsconfigPath: string = path.resolve(rootDir, 'tsconfig.json'),
 ) => {
   const { config, error } = ts.readConfigFile(tsconfigPath, ts.sys.readFile);
