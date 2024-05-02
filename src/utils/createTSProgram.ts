0b20ecb269e93704cab1e7e56c39db1ae2789478
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-02 17:43:59 +0300

    save

--- src/utils/createTSProgram.ts
+++ src/utils/createTSProgram.ts
@@ -3,8 +3,8 @@ import ts from 'typescript';
 import parseTSConfig from './parseTSConfig';
 
 const createTSProgram = (
-  rootDir: string = process.cwd(),
   testPath: string,
+  rootDir?: string,
   tsconfigPath?: string,
 ) => {
   const { config, error } = parseTSConfig(rootDir, tsconfigPath);
