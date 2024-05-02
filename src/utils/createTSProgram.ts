adfc758ef8e98cf274700bc9f82ea499d4e3b0e6
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-02 19:51:19 +0300

    save

--- src/utils/createTSProgram.ts
+++ src/utils/createTSProgram.ts
@@ -1,20 +1,23 @@
-import path from 'path';
-import tsconfig from 'get-tsconfig';
 import ts from 'typescript';
 
+import parseTSConfig from './parseTSConfig';
+
 const createTSProgram = (
-  rootDir: string = process.cwd(),
   testPath: string,
-  tsconfigPath: string = path.resolve(rootDir, 'tsconfig.json'),
+  rootDir?: string,
+  tsconfigPath?: string,
 ) => {
-  const config = tsconfig.parseTsconfig(tsconfigPath);
+  const { config, error } = parseTSConfig(rootDir, tsconfigPath);
 
-  console.log('config', config);
-
-  return ts.createProgram([testPath], {
+  const program = ts.createProgram([testPath], {
     noEmit: true,
-    ...config.compilerOptions,
+    ...config.options,
   });
+
+  return {
+    program,
+    error,
+  };
 };
 
 export default createTSProgram;
