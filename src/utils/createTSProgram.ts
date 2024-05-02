670d6dea414ceff7fbd07533e6ded9f761ccf5a7
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-02 16:46:33 +0300

    save

--- src/utils/createTSProgram.ts
+++ src/utils/createTSProgram.ts
@@ -1,40 +1,54 @@
 import path from 'path';
 import ts from 'typescript';
 
+import parseTSConfig from './parseTSConfig';
+
 const createTSProgram = (
   rootDir: string,
   testPath: string,
-  tsconfigPath: string = path.resolve(rootDir, 'tsconfig.json'),
+  tsconfigPath?: string,
 ) => {
-  const currentWorkDir = process.cwd();
+  // const currentWorkDir = process.cwd();
+  //
+  // const configFile = ts.findConfigFile(
+  //   currentWorkDir,
+  //   ts.sys.fileExists,
+  //   tsconfigPath,
+  // );
+  //
+  // if (configFile) {
+  //   const { config, error } = ts.readConfigFile(configFile, ts.sys.readFile);
+  //
+  //   const parsedConfig = ts.parseJsonConfigFileContent(
+  //     config,
+  //     ts.sys,
+  //     currentWorkDir,
+  //   );
+  //
+  //   const program = ts.createProgram([testPath], {
+  //     noEmit: true,
+  //     ...parsedConfig.options,
+  //   });
+  //
+  //   return {
+  //     program,
+  //     error,
+  //   };
+  // } else {
+  //   throw new Error(`Cannot find tsconfig file: ${tsconfigPath}`);
+  // }
 
-  const configFile = ts.findConfigFile(
-    currentWorkDir,
-    ts.sys.fileExists,
-    tsconfigPath,
-  );
+  const { config, error } = parseTSConfig(rootDir, tsconfigPath);
 
-  if (configFile) {
-    const { config, error } = ts.readConfigFile(configFile, ts.sys.readFile);
+  const program = ts.createProgram([testPath], {
+    noEmit: true,
+    ...config.options,
+  });
 
-    const parsedConfig = ts.parseJsonConfigFileContent(
-      config,
-      ts.sys,
-      currentWorkDir,
-    );
-
-    const program = ts.createProgram([testPath], {
-      noEmit: true,
-      ...parsedConfig.options,
-    });
-
-    return {
-      program,
-      error,
-    };
-  } else {
-    throw new Error(`Cannot find tsconfig file: ${tsconfigPath}`);
-  }
+  return {
+    program,
+    error,
+  };
 };
 
 export default createTSProgram;
