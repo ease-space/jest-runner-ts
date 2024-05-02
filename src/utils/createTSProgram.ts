c2911e8b60fe6480644186f31807f0dbd6407994
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-02 16:47:33 +0300

    save

--- src/utils/createTSProgram.ts
+++ src/utils/createTSProgram.ts
@@ -7,36 +7,6 @@ const createTSProgram = (
   testPath: string,
   tsconfigPath?: string,
 ) => {
-  // const currentWorkDir = process.cwd();
-  //
-  // const configFile = ts.findConfigFile(
-  //   currentWorkDir,
-  //   ts.sys.fileExists,
-  //   tsconfigPath,
-  // );
-  //
-  // if (configFile) {
-  //   const { config, error } = ts.readConfigFile(configFile, ts.sys.readFile);
-  //
-  //   const parsedConfig = ts.parseJsonConfigFileContent(
-  //     config,
-  //     ts.sys,
-  //     currentWorkDir,
-  //   );
-  //
-  //   const program = ts.createProgram([testPath], {
-  //     noEmit: true,
-  //     ...parsedConfig.options,
-  //   });
-  //
-  //   return {
-  //     program,
-  //     error,
-  //   };
-  // } else {
-  //   throw new Error(`Cannot find tsconfig file: ${tsconfigPath}`);
-  // }
-
   const { config, error } = parseTSConfig(rootDir, tsconfigPath);
 
   const program = ts.createProgram([testPath], {
