ff990e7432907f7d8bb7f272f14158aad71d5087
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-05 14:42:00 +0300

    save

--- src/utils/getDiagnosticLocation.ts
+++ src/utils/getDiagnosticLocation.ts
@@ -2,25 +2,36 @@ import { Diagnostic, getLineAndCharacterOfPosition } from 'typescript';
 
 const getDiagnosticLocation = (diagnostic: Diagnostic) => {
   if (diagnostic.file) {
-    const { line: lineStart, character: characterStart } =
-      getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
+    if (diagnostic.start) {
+      const { line: lineStart, character: characterStart } =
+        getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
 
-    const { line: lineEnd, character: characterEnd } =
-      getLineAndCharacterOfPosition(
-        diagnostic.file,
-        diagnostic.start + diagnostic.length,
-      );
+      if (diagnostic.length) {
+        const { line: lineEnd, character: characterEnd } =
+          getLineAndCharacterOfPosition(
+            diagnostic.file,
+            diagnostic.start + diagnostic.length,
+          );
 
-    return {
-      start: {
-        line: lineStart + 1,
-        column: characterStart + 1,
-      },
-      end: {
-        line: lineEnd + 1,
-        column: characterEnd + 1,
-      },
-    };
+        return {
+          start: {
+            line: lineStart + 1,
+            column: characterStart + 1,
+          },
+          end: {
+            line: lineEnd + 1,
+            column: characterEnd + 1,
+          },
+        };
+      }
+
+      return {
+        start: {
+          line: lineStart + 1,
+          column: characterStart + 1,
+        },
+      };
+    }
   }
 };
 
