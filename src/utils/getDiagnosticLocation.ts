14b463b10160af25d3d8399f1afd12f20e2be5f9
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-05 15:27:06 +0300

    save

--- src/utils/getDiagnosticLocation.ts
+++ src/utils/getDiagnosticLocation.ts
@@ -7,6 +7,13 @@ const getDiagnosticLocation = (diagnostic: Diagnostic) => {
     if (typeof start === 'number') {
       const lineAndCharacterStart = getLineAndCharacterOfPosition(file, start);
 
+      const location = {
+        start: {
+          line: lineAndCharacterStart.line + 1,
+          column: lineAndCharacterStart.character + 1,
+        },
+      };
+
       if (typeof length === 'number') {
         const lineAndCharacterEnd = getLineAndCharacterOfPosition(
           file,
@@ -14,10 +21,7 @@ const getDiagnosticLocation = (diagnostic: Diagnostic) => {
         );
 
         return {
-          start: {
-            line: lineAndCharacterStart.line + 1,
-            column: lineAndCharacterStart.character + 1,
-          },
+          ...location,
           end: {
             line: lineAndCharacterEnd.line + 1,
             column: lineAndCharacterEnd.character + 1,
@@ -25,12 +29,7 @@ const getDiagnosticLocation = (diagnostic: Diagnostic) => {
         };
       }
 
-      return {
-        start: {
-          line: lineAndCharacterStart.line + 1,
-          column: lineAndCharacterStart.character + 1,
-        },
-      };
+      return location;
     }
   }
 };
