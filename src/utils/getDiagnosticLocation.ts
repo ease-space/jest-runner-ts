0971b06fc498b7d7b8dca4e253335a6912fbc99f
Yurii Maistrenko <yurii_maistrenko@MacBook-Pro-Yurii.local>
2024-05-05 15:36:44 +0300

    save

--- src/utils/getDiagnosticLocation.ts
+++ src/utils/getDiagnosticLocation.ts
@@ -3,34 +3,32 @@ import { Diagnostic, getLineAndCharacterOfPosition } from 'typescript';
 const getDiagnosticLocation = (diagnostic: Diagnostic) => {
   const { file, start, length } = diagnostic;
 
-  if (file) {
-    if (typeof start === 'number') {
-      const lineAndCharacterStart = getLineAndCharacterOfPosition(file, start);
+  if (file && typeof start === 'number') {
+    const lineAndCharacterStart = getLineAndCharacterOfPosition(file, start);
 
-      const locationStart = {
-        start: {
-          line: lineAndCharacterStart.line + 1,
-          column: lineAndCharacterStart.character + 1,
+    const locationStart = {
+      start: {
+        line: lineAndCharacterStart.line + 1,
+        column: lineAndCharacterStart.character + 1,
+      },
+    };
+
+    if (typeof length === 'number') {
+      const lineAndCharacterEnd = getLineAndCharacterOfPosition(
+        file,
+        start + length,
+      );
+
+      return {
+        ...locationStart,
+        end: {
+          line: lineAndCharacterEnd.line + 1,
+          column: lineAndCharacterEnd.character + 1,
         },
       };
-
-      if (typeof length === 'number') {
-        const lineAndCharacterEnd = getLineAndCharacterOfPosition(
-          file,
-          start + length,
-        );
-
-        return {
-          ...locationStart,
-          end: {
-            line: lineAndCharacterEnd.line + 1,
-            column: lineAndCharacterEnd.character + 1,
-          },
-        };
-      }
-
-      return locationStart;
     }
+
+    return locationStart;
   }
 };
 
